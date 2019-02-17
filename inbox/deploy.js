2f32a3d1181a853d7cdc21aad5c2798514b1e29b
Rohit Maheshwari <rohit.maheshwari@sjsu.edu>
2019-02-16 20:49:17 -0800

    Deploying contract to Rinkby Test Network
    Added Truffle  wallet provider to get account details from mnemonic phrase
    Used Infura APIs to handle the deployment part

---
+++ inbox/deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'pink song seed ladder senior bar game oxygen chicken pilot sweet arrive',
+  'https://rinkeby.infura.io/v3/bbbfbae98c6f4ff48c0c8384071ecbfb'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  // const result = await new web3.eth.Contract(JSON.parse(interface))
+  //   .deploy({ data: bytecode, arguments: ['Hi there!'] })
+  //   .send({ gas: '1000000', from: accounts[0] });
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+     .deploy({data: '0x' + bytecode, arguments: ['Hi there!']}) // added 0x bytecode
+     .send({from: accounts[0]}); // removed gas
+
+  console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
